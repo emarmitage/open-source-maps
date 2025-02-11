6ee7164d8f64b868fec83380bc51833323f6e5fe
EARMITAG <emma.armitage@gov.bc.ca>
2025-02-11 09:14:45 -0800

    testing esri basemap

--- map.js
+++ map.js
@@ -1,7 +1,12 @@
+// ago access token
+const accessToken = "mzFcMRqhxzPAoRJavp2MJnlRO2BSOd3tUBeUnKd1MuMZtL8LTSJKXTZFussVqklx5SivTxKG9broW6ewdJtk-RIsbSVOwjwzm6jg3faMx7DleglNHSwEJ5gvvG60w2SS-WTZ9EeXYTI-ab6HOvHPTbhl9bqxYJJeKpI43olUnhc."
+const basemapEnum = 'arcgis/streets';
+
 // Initialize the map
 var map = new maplibregl.Map({
     container: 'map', // container id
     style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json', // style URL
+    // style: 'https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/${basemapEnum}?token=${accessToken}',
     center: [-127.647621, 53.726669], // starting position [lng, lat]
     zoom: 5, // starting zoom (adjusted to better fit the BC region)
 });
