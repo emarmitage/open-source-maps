ad3fc57ac9f9ac1c1bc30f19236fe27198241362
EARMITAG <emma.armitage@gov.bc.ca>
2025-02-13 16:07:37 -0800

    change basemap

--- map.js
+++ map.js
@@ -1,11 +1,12 @@
 // ago access token
-const accessToken = "mzFcMRqhxzPAoRJavp2MJnlRO2BSOd3tUBeUnKd1MuMZtL8LTSJKXTZFussVqklx5SivTxKG9broW6ewdJtk-YuTiU_vlfnVNL4fuVQ6TOFKLnNt4fW4n20WVARk4OyIWsdBaoLGibptF1F-bKxcubZix5A0gwyKYhLIN0C0yTOoW7SRW5iPSIcMiw3sZCLk"
-const basemapEnum = 'arcgis/outdoor';
+const accessToken = 'mzFcMRqhxzPAoRJavp2MJnlRO2BSOd3tUBeUnKd1MuMZtL8LTSJKXTZFussVqklx5SivTxKG9broW6ewdJtk-f0cU1TjbzfsRETZERaGx4tmRJzjlRdlKd-2MJov1dbTHYUqWJeADGFk1sJ6kOjUUDhPaj3uj8TLDtc6RD34BpRDPVZtYMu9PRKiRYiDZ1yDTDp8_hE0_1ZnigthWNQtSQ..'
+const basemapEnum = 'arcgis/outdoor'
 
 // Initialize the map
 var map = new maplibregl.Map({
     container: 'map', // container id
-    style: `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/${basemapEnum}?token=${accessToken}`, // style url
+    // style: `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/${basemapEnum}?token=${accessToken}`, // style url
+    style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
     center: [-127.647621, 53.726669], // starting position [lng, lat]
     zoom: 5, // starting zoom (adjusted to better fit the BC region)
 });
